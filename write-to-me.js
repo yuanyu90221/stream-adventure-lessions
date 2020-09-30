9df40e8e82772d5e7b423c6c2ad86a8d2bc33696
json <yuanyu90221@gmail.com>
2020-09-30 10:59:24 +0800

    add write-to-me

---
+++ write-to-me.js
@@ -0,0 +1,9 @@
+const { Writable } = require('stream');
+class MyWritable extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(`writing: ` + chunk.toString());
+    callback();
+  }
+}
+const stream = new MyWritable();
+process.stdin.pipe(stream);
