4df6a798a14da5887b09c750e46d7c2a5807f15b
jsonLiang <yuanyu90221@gmail.com>
2020-10-06 16:00:20 +0800

    add combiner.js

--- combiner.js
+++ combiner.js
@@ -1,10 +1,29 @@
 const combine = require('stream-combiner');
 const split2 = require('split2');
-const total = [];
+const zlib = require('zlib');
+const through2 =  require('through2');
+const result = {};
 module.exports = function () {
- return combine(split2, function(line, _, next) => {
-    if (line !== null) {
-      total.push(line);
+  let current;
+  const write = function (line, _, next) {
+    if (line.length === 0) return next();
+    const row = JSON.parse(line);
+    if (row.type === 'genre') {
+      if (current) {
+        this.push(JSON.stringify(current)+'\n');
+      }
+      current = { name: row.name, books: []};
+    } else if (row.type === 'book') {
+      current.books.push(row.name);
     }
- })
+    next();
+  };
+  const end = function (next) {
+    if (current) {
+      this.push(JSON.stringify(current) + '\n');
+    }
+    next();
+  };
+  const grouper = through2(write, end);
+ return combine(split2(), grouper, zlib.createGzip())
 };
